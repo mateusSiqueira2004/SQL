eafc61ff988a353cdf0e8bb93ea31f37ba3bdc67
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-09-18 12:36:23 -0300

    Atividade 1

--- DB-SQL-SERVER-02_Atividade1/Atividade1.sql
+++ DB-SQL-SERVER-02_Atividade1/Atividade1.sql
@@ -7,7 +7,7 @@ CREATE TABLE tb_colaboradores(
 	Nome VARCHAR(100) NOT NULL,
 	Cargo VARCHAR(255) NOT NULL,
 	Idade INT NOT NULL,
-	Salario DECIMAL NOT NULL
+	Salario DECIMAL (9,2) NOT NULL
 );
 
 INSERT INTO tb_colaboradores(Nome,Cargo,Idade,Salario) VALUES ('Jovem1', 'Analista de Sistema',35,5200.00);
@@ -16,7 +16,7 @@ INSERT INTO tb_colaboradores(Nome,Cargo,Idade,Salario) VALUES ('Jovem3', 'Desenv
 INSERT INTO tb_colaboradores(Nome,Cargo,Idade,Salario) VALUES ('Jovem4', 'Estagiario',18,1250.00);
 INSERT INTO tb_colaboradores(Nome,Cargo,Idade,Salario) VALUES ('Jovem5', 'Surpevisor',65,15900.00);
 
-SELECT * FROM tb_colaboradores where Salario > 2000;
-SELECT * FROM tb_colaboradores where Salario < 2000;
+SELECT * FROM tb_colaboradores where Salario > 2000.00;
+SELECT * FROM tb_colaboradores where Salario < 2000.00;
 
 UPDATE tb_colaboradores SET Idade = 19 WHERE Nome = 'Jovem4' AND Cargo = 'Estagiario';
